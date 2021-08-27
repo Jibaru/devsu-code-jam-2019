f316becd97d7be312086f1135f3fb1fc53d634ef
Jibaru <ignacioruedaboada@hotmail.com>
2021-08-26 22:41:16 -0500

    Problem 06 fixed

--- src/06.ts
+++ src/06.ts
@@ -1,7 +1,7 @@
 const solve06 = (times: string[]) => {
-  // const HHAND_ANGLE_PER_HOUR = 30.0;
-  // const MHAND_ANGLE_PER_MINUTE = 6.0;
-  // const HHAND_ANGLE_PER_MINUTE = 0.1;
+  const HHAND_ANGLE_PER_HOUR = 30.0;
+  const MHAND_ANGLE_PER_MINUTE = 6.0;
+  const HHAND_ANGLE_PER_MINUTE = 0.5;
 
   const isInFormat = (time: string): boolean => /^\d{2}:\d{2}$/.test(time);
   const hasCorrectTime = (hh: any, mm: any) => {
@@ -24,25 +24,19 @@ const solve06 = (times: string[]) => {
       anglesSum -= 100;
       continue;
     }
+    if (hh >= 12) hh -= 12;
 
-    /*const hhIncrement = mm * HHAND_ANGLE_PER_MINUTE;
+    const hhIncrement = mm * HHAND_ANGLE_PER_MINUTE;
     hh = hh * HHAND_ANGLE_PER_HOUR;
     mm = mm * MHAND_ANGLE_PER_MINUTE;
 
-    if (hh >= mm) anglesSum = anglesSum + hh + hhIncrement - mm;
-    else anglesSum = anglesSum + mm - hh - hhIncrement;*/
+    if (hh > mm) anglesSum = anglesSum + (360 - (hh + hhIncrement - mm));
+    else anglesSum = anglesSum + mm - hh - hhIncrement;
 
-    hh = (hh % 12) + mm * (1 / 60);
-    mm = mm / 5;
-
-    if (hh <= mm) {
-      anglesSum += (mm - hh) * 30;
-    } else {
-      anglesSum += (12 - (hh - mm)) * 30;
-    }
   }
 
   return anglesSum;
 };
 
+console.log(solve06(["12:00"]));
 console.log(solve06(["12:00", "17:30", "blabla", "20:21", "26:88"]));
